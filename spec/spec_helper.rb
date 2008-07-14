2ed3a0ac3550938f0b1edf9aee67812c0a1fce8f
Nathan Fixler <nathan@fixler.org>
2008-07-13 23:15:47 -0400

    Cleaned up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,3 @@
 require 'rubygems'
 require 'spec'
 require File.dirname(__FILE__) + '/../lib/nikeplus'
-
-Spec::Runner.configure do |config|
-  # == Mock Framework
-  #
-  # RSpec uses it's own mocking framework by default. If you prefer to
-  # use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-end
