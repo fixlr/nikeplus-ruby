787c18defede25a483b4f4e06fc0051b3b31590c
Nathan Fixler <nathan@fixler.org>
2008-07-24 12:20:51 -0400

    Fetch recent runs

--- lib/nikeplus/base.rb
+++ lib/nikeplus/base.rb
@@ -8,7 +8,7 @@ module NikePlus
     
     BASE_URL = 'https://secure-nikeplus.nike.com/nikeplus/v1/services'
     DATA_URL = BASE_URL + '/app/get_user_data.jhtml'
-    LIST_URL = BASE_URL + '/app/run_list.jhtml'
+    RUNS_LIST_URL = BASE_URL + '/app/run_list.jhtml'
     RUN_URL  = BASE_URL + '/app/get_run.jhtml'
     GOAL_URL = BASE_URL + '/app/goal_list.jhtml'
     CHALLENGES_URL = BASE_URL + '/widget/get_challenges_for_user.jhtml'
@@ -18,6 +18,7 @@ module NikePlus
       # Authenticate
       @session = NikePlus::Session.new
       @session.authenticate(login, password)
+      @runs = nil
     end
     
     def whoami
@@ -25,7 +26,17 @@ module NikePlus
     end
     
     def runs
-      @runs ||= RunList.new(@session)
+      @runs ||= fetch_runs_list
+    end
+    
+    private
+    def fetch_runs_list
+      list = []
+      response = @session.send_request(RUNS_LIST_URL)
+      response.fetch('runList').each_element_with_text do |run_xml|
+        list << NikePlus::Run.new(run_xml)
+      end
+      list
     end
   end
 end
