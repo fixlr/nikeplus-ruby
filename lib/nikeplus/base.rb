c4c89bfc6779b6ad6af18506c899439569f58f93
bob-p <thomas@thomas-pomfrets-macbook.local>
2009-01-08 21:58:04 +0000

    added support for fetching runs from your xml feed this implentation is not complete but you can get your runs without giving up your pw!

--- lib/nikeplus/base.rb
+++ lib/nikeplus/base.rb
@@ -13,24 +13,46 @@ module NikePlus
     GOAL_URL = BASE_URL + '/app/goal_list.jhtml'
     CHALLENGES_URL = BASE_URL + '/widget/get_challenges_for_user.jhtml'
     CHALLENGES_DETAIL_URL = BASE_URL + '/app/get_challenge_detail.jhtml'
+    XML_BASE_URL = 'http://nikeplus.nike.com/nikeplus/v1/services/widget/get_public_run_list.jsp?userID='
     
-    def initialize(login, password)
+    def initialize(login, password, uid=nil)
+      @uid = uid
       # Authenticate
-      @session = NikePlus::Session.new
-      @session.authenticate(login, password)
+      unless @uid
+        @session = NikePlus::Session.new
+        @session.authenticate(login, password)
+      end
       @runs = nil
     end
     
     def runs
-      @runs ||= fetch_runs_list
+      @runs ||= fetch_runs
     end
     
     private
+    def fetch_runs
+      @uid ? fetch_runs_from_xml : fetch_runs_list
+    end
+    
     def fetch_runs_list
       list = []
       response = @session.send_request(RUNS_LIST_URL)
       response.fetch('runList').each_element_with_text do |run_xml|
         list << NikePlus::Run.new(run_xml)
+      end
+      list
+    end
+    
+    def fetch_runs_from_xml
+      #TODO I think these feed functions should belong in their own class
+      uri = URI.parse(XML_BASE_URL + @uid.to_s)
+      http = Net::HTTP.start(uri.host)
+      doc = REXML::Document.new(http.get(uri.request_uri).body)
+      
+      list = []
+      
+      doc.root.elements['runList'].each_element_with_text do |run|
+        list << NikePlus::Run.new(run)
       end
       list
     end
