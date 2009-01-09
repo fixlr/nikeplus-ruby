a0f46fe11c1d3c3e5b21fab19ab1bd5ba7972591
Thomas <thomaspomfret@gmail.com>
2009-01-09 17:29:20 +0000

    some work on specs

--- spec/nikeplus/profile_spec.rb
+++ spec/nikeplus/profile_spec.rb
@@ -1,4 +1,16 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "NikePlus::Profile" do
+  before(:all) do
+    profile = REXML::Document.new(File.read(File.dirname(__FILE__) + "/../xml/login_successful.xml"))
+    @profile = NikePlus::Profile.new(profile.root.elements['profile'])
+  end
+  
+  it "should set the id" do
+    @profile.id.should == "123456"
+  end
+  
+  it "should set the first name" do
+    @profile.firstName.should == "John"
+  end
 end
--- spec/nikeplus/session_spec.rb
+++ spec/nikeplus/session_spec.rb
@@ -2,24 +2,22 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "NikePlus::Session" do
 
-  #TODO update session specs for new structure
-  it "should authenticate on initialization" do
-    pending
+  it "should call authenticate on initialization" do
+    NikePlus::Session.any_instance.expects(:authenticate).with('john@example.com', 'secret').once.returns(true)
+    
+    session = NikePlus::Session.new('john@example.com', 'secret')
   end
   
   it "should fetch runs from API" do
-    pending
-  #   mock_session = mock(NikePlus::Session)
-  #   NikePlus::Session.should_receive(:new).and_return(mock_session)
-  #   mock_session.should_receive(:authenticate).with('john@example.com', 'secret').once.and_return(true)
-  # 
-  #   raw_response = mock("RawResponse")
-  #   raw_response.should_receive(:fetch).with('set-cookie').and_return("COOKIE")
-  #   raw_response.should_receive(:body).and_return(File.read(File.dirname(__FILE__) + "/../xml/runs_list.xml"))
-  #   
-  #   mock_session.should_receive(:send_request).with(NikePlus::Base::RUNS_LIST_URL).and_return(NikePlus::HttpResponse.new(raw_response))
-  #   
-  #   nikeplus = NikePlus.new('john@example.com', 'secret')
-  #   nikeplus.runs
+    NikePlus::Session.any_instance.expects(:authenticate).with('john@example.com', 'secret').once.returns(true)
+    
+    raw_response = mock("RawResponse")
+    raw_response.expects(:body).returns(File.read(File.dirname(__FILE__) + "/../xml/runs_list.xml"))
+    
+    NikePlus::Session.any_instance.expects(:send_request).returns(NikePlus::HttpResponse.new(raw_response))
+    NikePlus::Run.expects(:new).times(10)
+    
+    session = NikePlus::Session.new('john@example.com', 'secret')
+    session.runs
   end
 end
